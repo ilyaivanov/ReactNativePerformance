947233de962c5a614eec76c39a291b927558720c
Ilya <illiai@wix.com>
2019-04-08 17:41:32 +0300

    Add unperformant version

--- src/2. Careless Renders/index.js
+++ src/2. Careless Renders/index.js
@@ -1,19 +1,124 @@
 import React from "react";
-import {Text, View} from "react-native";
+import {ActivityIndicator, Dimensions, FlatList, StyleSheet, View} from "react-native";
+import ArtistInfo, {SELECTION_COLOR} from "../ArtistInfo";
+import {lastFM} from '../../apiKeys';
+
+const mapArtist = (artist, maxListeners) => ({
+  name: artist.name,
+  listeners: Number(artist.listeners),
+  id: artist.mbid,
+  percent: Math.floor((Number(artist.listeners) / maxListeners) * 100),
+  image: artist.image[3]["#text"],
+});
+
+const updatePercents = artists => {
+  const max = Math.max(...artists.map(a => a.listeners));
+
+  return artists.map(a => ({
+    ...a,
+    percent: (Math.floor(a.listeners) / max) * 100,
+  }));
+};
 
 class CarelessRenders extends React.Component {
   static navigationOptions = {
-    title: 'Careless List',
+    title: "Careless FlatList",
+  };
+
+  state = {
+    artists: [],
+    currentPage: 0,
+    selected: {},
+  };
+
+  componentDidMount() {
+    this.loadMoreItems();
+  }
+
+  onPress = id =>
+    this.setState({
+      selected: {
+        ...this.state.selected,
+        [id]: !this.state.selected[id],
+      },
+    });
+
+  loadMoreItems = () => {
+    const newPage = this.state.currentPage + 1;
+    this.setState({currentPage: newPage});
+    console.log(`Fetching 50 more items for ${newPage}th page`);
+    fetch(
+      `http://ws.audioscrobbler.com/2.0/?method=geo.getTopArtists&country=lithuania&api_key=${lastFM}&format=json&limit=50&page=${newPage}`,
+    )
+      .then(res => res.json())
+      .then(response => {
+        const newArtists = this.state.artists.concat(
+          response.topartists.artist.map(mapArtist),
+        );
+        this.setState({artists: updatePercents(newArtists)});
+      });
   };
 
   render() {
+    const size = (Dimensions.get("window").width - ITEM_PADDING * 6) / 3;
     return (
-      <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-        <Text>To Be Defined...</Text>
+      <View style={{flex: 1}}>
+        <FlatList
+          contentContainerStyle={[
+            s.listContainer,
+            this.state.artists.length === 0 && s.emptyList,
+          ]}
+          data={this.state.artists}
+          keyExtractor={ar => ar.id}
+          numColumns={3}
+          onEndReached={this.loadMoreItems}
+          onEndReachedThreshold={0.1}
+          ListFooterComponent={
+            <ActivityIndicator
+              style={{paddingBottom: ITEM_PADDING}}
+              size="large"
+              color={SELECTION_COLOR}
+            />
+          }
+          renderItem={({item, index}) => (
+            <ArtistInfo
+              style={[s.artistContainer, index % 3 === 0 && s.firstArtistInRow]}
+              size={size}
+              percent={item.percent}
+              id={item.id}
+              onPress={() => this.onPress(item.id)}
+              isSelected={this.state.selected[item.id]}
+              listeners={item.listeners}
+              name={item.name}
+              url={item.image}
+            />
+          )}
+        />
       </View>
     );
   }
 }
 
+const ITEM_PADDING = 10;
+
+const s = StyleSheet.create({
+  listContainer: {
+    paddingLeft: ITEM_PADDING * 2,
+    paddingRight: ITEM_PADDING * 2,
+    paddingTop: ITEM_PADDING * 2,
+  },
+  emptyList: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+  },
+  artistContainer: {
+    marginLeft: ITEM_PADDING,
+    marginBottom: ITEM_PADDING,
+  },
+  firstArtistInRow: {
+    marginLeft: undefined,
+  },
+});
 
 export default CarelessRenders;
--- src/ArtistInfo.js
+++ src/ArtistInfo.js
@@ -1,49 +1,54 @@
 import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React from 'react';
+import tinycolor2 from 'tinycolor2';
 
-export default function ArtistInfo({id, size,url,  listeners, percent, name, isSelected, onPress}) {
+export default function ArtistInfo({style, id, size, url, listeners, percent, name, isSelected, onPress}) {
   console.log('ArtistInfo.render');
-  return <TouchableOpacity onPress={() => onPress(id)} style={{width: size}}>
+  return <TouchableOpacity onPress={() => onPress(id)} style={[{width: size}, style]}>
     <View style={{width: size, height: size}}>
       <Image style={{width: size, height: size, borderRadius: BORDER_RADIUS}}
              source={{uri: url}}/>
 
       <View style={[s.bar]}>
-        <Text style={s.listeners}>{numberWithSpaces(listeners)}</Text>
-        <View style={[s.bar, s.filledBar, {width: size * (percent / 100)}]}>
+        <Text style={s.listeners} numberOfLines={1} ellipsizeMode='tail'>{name}</Text>
+        <View style={[s.bar, s.filledBar, percent < 95 && s.intermediatePercent, {width: size * (percent / 100)}]}>
         </View>
       </View>
       {isSelected && <View style={s.selectedBorder}/>}
     </View>
-    <View>
-      <Text style={[s.artistTitle, isSelected && s.selectedTitle]}>{name}</Text>
-    </View>
   </TouchableOpacity>;
 }
 
-const SELECTION_COLOR = 'green';
-const BORDER_RADIUS = 6;
+export const SELECTION_COLOR = 'purple';
+const BORDER_RADIUS = 10;
 
 const s = StyleSheet.create({
   bar: {
     ...StyleSheet.absoluteFillObject,
     top: undefined,
-    height: 30,
+    height: 25,
     borderBottomLeftRadius: BORDER_RADIUS,
     borderBottomRightRadius: BORDER_RADIUS,
-    backgroundColor: 'rgba(0,0,0,0.3)',
+    backgroundColor: 'rgba(0,0,0,0.4)',
     justifyContent: 'center',
-    paddingLeft: 10,
+    paddingLeft: 5,
+    paddingRight: 5,
   },
   filledBar: {
     zIndex: -1,
-    backgroundColor: 'rgba(0,0,0,0.4)',
+    backgroundColor: tinycolor2(SELECTION_COLOR).setAlpha(0.6).toRgbString(),
+    overflow: 'hidden',
+  },
+  intermediatePercent: {
+    borderBottomRightRadius: 0,
   },
   listeners: {
-    color: 'white', fontWeight: '600', fontSize: 16,
+    color: 'white',
+    fontWeight: '600',
+    fontSize: 14,
   },
   artistTitle: {
-    fontSize: 21, textAlign: 'center',
+    fontSize: 18, textAlign: 'center',
   },
   selectedTitle: {
     color: SELECTION_COLOR,
